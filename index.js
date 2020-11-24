ad51c339628e96e75cdb5e2105e50336bbace02d
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-11-24 16:31:18 +0300

    finished

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const CharacterCounterInput = ({ text, defaults }) => {
-	const maxLength = 280;
+	const maxLength = 28;
+	const [mood,setMood] = useState("");
+
+	function addTextToMood(badgeText) {
+		setMood(prev => prev += badgeText);
+	}
 
 	return (
-		<div className="counterInput">
+		<div className={`counterInput ${mood.length >= maxLength ? "tooLong" : ""}`} >
 			<div>
 				{defaults.map(b => {
-					return <button key={b}>{b}</button>;
+					return <button onClick={() => addTextToMood(b)} key={b}>{b}</button>;
 				})}
 			</div>
-			<textarea placeholder={text} />
-			<div>0/{maxLength}</div>
+			<textarea 
+				placeholder={text} 
+				value={mood} 
+				onChange={(e) => setMood(e.target.value)} 
+				disabled={mood.length >= maxLength ? "disabled" : ""}
+			/>
+			<div>{mood.length}/{maxLength}</div>
 		</div>
 	);
 };
